aa65173cf43652ad6f1e83034d3d1eadfb657f37
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-09-02 15:24:02 +0900

    formatting

--- lolchess/kakaobot/views.py
+++ lolchess/kakaobot/views.py
@@ -5,41 +5,59 @@ from rest_framework import status
 from rest_framework.request import Request
 import requests
 import json
-
+from django.http import JsonResponse
 class Message(APIView) :
 
     def get_summoner_id(self,nickname):
-        api =  'https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+nickname+'?api_key=RGAPI-191a0cbf-9630-4561-8601-d916fe96df37'
+        api ='https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+nickname+'?api_key=RGAPI-f4f16fe6-015f-4471-b99d-e6235bc452d2'
         data = requests.get(api)
-        return data
+        if data.status_code != 200:
+            return False
+        return data.json()
 
     def get_summoner_data(self,encrypt_id):
-        api = 'https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/'+encrypt_id+'?api_key=RGAPI-191a0cbf-9630-4561-8601-d916fe96df37'
+        api = 'https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/'+encrypt_id+'?api_key=RGAPI-f4f16fe6-015f-4471-b99d-e6235bc452d2'
         data = requests.get(api)
-        return data
+        return data.json()
 
     def post(self, request , format=None):
         data = request.data
         user_key = data['user_key']
         types = data['type']
         content = data['content']
-        if types == '대화하기':
-            return Response(status=status.HTTP_200_OK,data=types)
+        result = {"message":{"text":"닉네임을 입력하세요"}}
+        if content == '대화하기':
+            return Response(
+                    status=status.HTTP_200_OK,data=result)
 
         summoner = self.get_summoner_id(content)
-        encrypt_id = summoner.json()['id']
+        if summoner is False:
+            return Response(data={'message':{'text':'사용자가 존재하지 않습니다.'}})
+        encrypt_id = summoner['id']
         summoner_data = self.get_summoner_data(encrypt_id)
-        # summoner_data_json = json.dumps(summoner_data)
-        return Response(status=status.HTTP_200_OK,data=summoner_data.json()[2])
-
-
+        for i in summoner_data:
+            if i['queueType'] =='RANKED_TFT':
+                result2 = {
+                    'message':
+                        {
+                            'text':f'소환사이름 : {i["summonerName"]}\n티어 : {i["tier"]} {i["rank"]}\t{i["leaguePoints"]}\n승리 : {i["wins"]}\n패배 : {i["losses"]}'
+                        }
+                }
+                return Response(status=status.HTTP_200_OK,data=result2)
+        return Response(
+            data={
+                'message':{
+                    'text':'전적 검색 결과가 없습니다.'
+                }
+            }
+        )
 
 class Keyboard(APIView):
 
     def get(self, request , format=None):
         return Response({
             'type':'buttons',
-            'buttons':['1','2']
+            'buttons':['대화하기']
         })
 
-# Create your views here
+
