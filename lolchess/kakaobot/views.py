33ffbeb06010d20ec17939a6628754df9f30e1ef
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-09-02 22:30:24 +0900

    message Return

--- lolchess/kakaobot/views.py
+++ lolchess/kakaobot/views.py
@@ -19,26 +19,26 @@ class Message(APIView) :
         data = requests.get(api)
         return data.json()
 
-    def summoner_result(self, content):
-
-        summoner = self.get_summoner_id(content)
-        encrypt_id = summoner['id']
-        summoner_data = self.get_summoner_data(encrypt_id)
-
-        for i in summoner_data:
-            if i['queueType'] == 'RANKED_TFT':
-                return Response(data={
-                    'message':
-                        {
-                            'text': f'소환사이름 : {i["summonerName"]}\n티어 : {i["tier"]} {i["rank"]}\t{i["leaguePoints"]}\n승리 : {i["wins"]}\n패배 : {i["losses"]}'
-                        }
-                })
-
-        return Response(data={
-            'message': {
-                'text': '전적 검색 결과가 없습니다.'
-            }
-        })
+    # def summoner_result(self, content):
+    #
+    #     summoner = self.get_summoner_id(content)
+    #     encrypt_id = summoner['id']
+    #     summoner_data = self.get_summoner_data(encrypt_id)
+    #
+    #     for i in summoner_data:
+    #         if i['queueType'] == 'RANKED_TFT':
+    #             return Response(data={
+    #                 'message':
+    #                     {
+    #                         'text': f'소환사이름 : {i["summonerName"]}\n티어 : {i["tier"]} {i["rank"]}\t{i["leaguePoints"]}\n승리 : {i["wins"]}\n패배 : {i["losses"]}'
+    #                     }
+    #             })
+    #
+    #     return Response(data={
+    #         'message': {
+    #             'text': '전적 검색 결과가 없습니다.'
+    #         }
+    #     })
 
     def post(self, request , format=None):
         data = request.data
@@ -52,45 +52,55 @@ class Message(APIView) :
                 'message': {
                     'text': '소환사 이름을 입력하세요'
                 },
-                'keyboard':{
-                    "type": "buttons",
-                    "buttons": [
-                        "처음으로",
-                        "아이템",
-                        "시너지"
-                    ]
-                }
+                # 'keyboard':{
+                #     "type": "buttons",
+                #     "buttons": [
+                #         "처음으로",
+                #         "아이템",
+                #         "시너지"
+                #     ]
+                # }
             })
-        elif content == '시너지':
-            pass
-        elif content == '아이템':
-            pass
-        else :
+        # elif content == '시너지':
+        #     return Response(data={
+        #         'message': {
+        #             'text': '롤토체스 시너지 보기'
+        #         },
+        #         'keyboard':{
+        #             'type':'buttons',
+        #             'buttons':[
+        #                 '롤체지지 사이'
+        #             ]
+        #         }
+        #     })
+        # elif content == '아이템':
+        #     pass
+        # else :
 
-            summoner = self.get_summoner_id(content)
-            if summoner == False :
-                return Response(data={
-                    'message':{
-                        'text': '소환사가 존재하지 않습니다.'
-                    }
-                })
-            encrypt_id = summoner['id']
-            summoner_data = self.get_summoner_data(encrypt_id)
-
-            for i in summoner_data:
-               if i['queueType'] == 'RANKED_TFT':
-                    return Response(data={
-                        'message':
-                            {
-                                'text': f'소환사이름 : {i["summonerName"]}\n티어 : {i["tier"]} {i["rank"]}\t{i["leaguePoints"]}\n승리 : {i["wins"]}\n패배 : {i["losses"]}'
-                            }
-                    })
-
+        summoner = self.get_summoner_id(content)
+        if summoner == False :
             return Response(data={
-                'message': {
-                    'text': '전적 검색 결과가 없습니다.'
+                'message':{
+                    'text': '소환사가 존재하지 않습니다.'
                 }
             })
+        encrypt_id = summoner['id']
+        summoner_data = self.get_summoner_data(encrypt_id)
+
+        for i in summoner_data:
+           if i['queueType'] == 'RANKED_TFT':
+                return Response(data={
+                    'message':
+                        {
+                            'text': f'소환사이름 : {i["summonerName"]}\n티어 : {i["tier"]} {i["rank"]}\t{i["leaguePoints"]}\n승리 : {i["wins"]}\n패배 : {i["losses"]}'
+                        }
+                })
+
+        return Response(data={
+            'message': {
+                'text': '전적 검색 결과가 없습니다.'
+            }
+        })
 
 
 
@@ -99,5 +109,5 @@ class Keyboard(APIView):
     def get(self, request , format=None):
         return Response({
             'type':'buttons',
-            'buttons':['소환사 검색','아이템','시너지']
+            'buttons':['소환사 검색']
         })
