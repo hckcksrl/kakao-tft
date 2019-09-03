c5f17ef95f4b2d82681f520f9bc4691cd0010a22
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-09-03 14:38:39 +0900

    profile icon

--- lolchess/kakaobot/views.py
+++ lolchess/kakaobot/views.py
@@ -27,63 +27,17 @@ class Message(APIView) :
         data = requests.get(api)
         return data.json()
 
-    # def summoner_result(self, content):
-    #
-    #     summoner = self.get_summoner_id(content)
-    #     encrypt_id = summoner['id']
-    #     summoner_data = self.get_summoner_data(encrypt_id)
-    #
-    #     for i in summoner_data:
-    #         if i['queueType'] == 'RANKED_TFT':
-    #             return Response(data={
-    #                 'message':
-    #                     {
-    #                         'text': f'소환사이름 : {i["summonerName"]}\n티어 : {i["tier"]} {i["rank"]}\t{i["leaguePoints"]}\n승리 : {i["wins"]}\n패배 : {i["losses"]}'
-    #                     }
-    #             })
-    #
-    #     return Response(data={
-    #         'message': {
-    #             'text': '전적 검색 결과가 없습니다.'
-    #         }
-    #     })
 
     def post(self, request , format=None):
         data = request.data
-        user_key = data['user_key']
-        types = data['type']
         content = data['content']
 
         if content == '소환사 검색':
             return Response(data={
                 'message': {
                     'text': '소환사 이름을 입력하세요'
-                },
-                'keyboard': button
-                # 'keyboard':{
-                #     "type": "buttons",
-                #     "buttons": [
-                #         "처음으로",
-                #         "아이템",
-                #         "시너지"
-                #     ]
-                # }
+                }
             })
-        # elif content == '시너지':
-        #     return Response(data={
-        #         'message': {
-        #             'text': '롤토체스 시너지 보기'
-        #         },
-        #         'keyboard':{
-        #             'type':'buttons',
-        #             'buttons':[
-        #                 '롤체지지 사이'
-        #             ]
-        #         }
-        #     })
-        # elif content == '아이템':
-        #     pass
-        # else :
 
         summoner = self.get_summoner_id(content)
         if summoner == False :
@@ -107,14 +61,12 @@ class Message(APIView) :
                             },
                             'text': f'소환사이름 : {i["summonerName"]}\n티어 : {i["tier"]} {i["rank"]}\t{i["leaguePoints"]}점\n승리 : {i["wins"]}\n패배 : {i["losses"]}'
                         },
-                    'keyboard':button
                 })
 
         return Response(data={
             'message': {
                 'text': '전적 검색 결과가 없습니다.'
             },
-            'keyboard': button
         })
 
 
